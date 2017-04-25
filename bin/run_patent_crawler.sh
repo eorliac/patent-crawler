5383e98369323765c47fccb209b917cfc97c3743
Etienne Orliac <etienne@orliac.com>
2017-04-25 15:38:02 +0200

    Added bin dir and Docker configuration file

---
+++ bin/run_patent_crawler.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+#
+# Utility script
+#
+# E. Orliac, SCITAS, EPFL
+# 12/04/2017
+#
+# Build crawler, recreate ES indices, launch seed injection topology, kill it
+# then start the patent-crawler
+# ---------------------------------------------------------------------------
+
+echo "EXE: mvn clean package"
+echo ""
+#mvn clean package
+
+echo "CREATE INDICES"
+echo ""
+./conf/ES_create_indices.sh
+echo "\n\n"
+
+echo "\n\nLAUNCHING INJECTION TOPOLOGY"
+storm jar target/patent-crawler-1.0.jar com.digitalpebble.stormcrawler.elasticsearch.ESSeedInjector ~/patent-crawler/seeds/ patent-seeds.txt -conf conf/es-conf.yaml -conf conf/crawler-conf.yaml -local &
+
+
+echo "\n\nWAIT FOR INJECTION OF SEED URLs TO COMPLETE (Note: wait for 1 only)"
+i=0
+while [ $i -lt 1 ]
+do
+    i=$(curl --silent -XGET 'localhost:9200/status/_count?pretty' -H 'Content-Type: application/json' -d'
+{
+    "query": {
+        "query_string": {
+            "query": "*"
+        }
+    }
+}
+' | grep count | cut -d, -f 1 | cut -d: -f2)
+
+    if [ $i==0 ]; then
+       echo "  **** Still no seed URLs in spout. Will sleep for 10s.\n"
+       sleep 10
+    fi
+done
+
+echo "\nKILLING INJECTION TOPOLOGY (seed URL(s) found)"
+####storm kill  com.digitalpebble.stormcrawler.elasticsearch.ESSeedInjector
+kill -9 $(ps aux | grep '[e]lasticsearch.ESSeedInjector' | awk '{print $2}')
+
+echo "\nLAUNCHING PATENT-CRAWLER!!"
+storm jar target/patent-crawler-1.0.jar ch.epfl.scitas.patentcrawler.CrawlTopology -conf conf/es-conf.yaml -conf conf/crawler-conf.yaml -local
+
